a44566bc2b8f0dd618dc361a7798301f4d08f653
Kimbareeba <36816635+joshbaz@users.noreply.github.com>
2021-03-05 19:31:09 +0300

    HomeUpdate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GlobalStyle from "./globalStyles";
 import Home from "./views";
-import Services from './views/Services';
-import Ecommerce from './views/Commerce';
-import Branding from "./views/Branding";
-import  Work  from "./views/Work";
-import VisualIdentity from "./views/identity";
-import Esources from "./views/Esources";
-import Contact from "./views/Contact";
-import ScrollTop from "./components/ScrollToTop";
-import page404 from "./views/404";
+
 
 function App() {
   return (
@@ -19,14 +11,6 @@ function App() {
         <GlobalStyle />
         <Switch>
           <Route path="/" component={Home} exact />
-          <Route path="/services" component={Services} exact />
-          <Route path="/ecommerce" component={Ecommerce} exact />
-          <Route path="/branding" component={Branding} exact />
-          <Route path="/work" component={Work} exact />
-          <Route path="/visual" component={VisualIdentity} exact />
-          <Route path="/esources" component={Esources} exact />
-          <Route path="/contact" component={Contact} exact />
-          <Route component={page404} exact/>
         </Switch>
       </Router>
     </>
